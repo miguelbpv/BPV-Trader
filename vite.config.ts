0d22c2cf4f9adb9fa84d9b0cb04927a661376a32
miguelbpv <onlinembpv@gmail.com>
2025-08-08 15:23:38 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/BPV-Trader/',        // 👈 importante para GitHub Pages (project page)
   build: { outDir: 'dist' },
   server: { port: 5173 }
 })
